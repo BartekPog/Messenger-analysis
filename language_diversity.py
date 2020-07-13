ee32c9898f32e97b22174432215bfb9d184a4194
Bartłomiej Pogodziński <bartek.pogod@gmail.com>
2020-07-14 00:08:44 +0200

    Add language diversity rank (#6)
    * Add spacy testing and first transformers
    
    * Add language diversity rank

---
+++ language_diversity.py
@@ -0,0 +1,83 @@
+import spacy
+from spacy.lang.xx import MultiLanguage
+import numpy as np
+import pandas as pd
+import math
+
+LANGUAGE = "polish"
+
+LANGUAGE_MODELS = {
+    "polish": "pl_core_news_md",
+    "english": "en_core_web_sm"
+}
+
+
+def getModel(language: str, language_models: dict = LANGUAGE_MODELS) -> spacy.lang:
+
+    if language not in language_models.keys():
+        print("language not supported. Running on MultiLanguage.")
+        return MultiLanguage()
+
+    return spacy.load(language_models[language], disable=["parser"])
+
+
+def removeEntities(doc: spacy.tokens.Doc) -> list:
+    entityRanges = np.concatenate(
+        [np.arange(e.start, e.end) for e in doc.ents])
+
+    return [token for token in doc if token.i not in entityRanges]
+
+
+def removePunctuation(doc: list) -> list:
+    return [token for token in doc if token.pos_ != "PUNCT"]
+
+
+def removeNumbers(doc: list) -> list:
+    return [token for token in doc if token.pos_ != "NUM"]
+
+
+def calculateDiversity(doc: spacy.tokens.Doc, batch_size: int = 2000) -> float:
+    noEnts = removeEntities(doc)
+    noPunct = removePunctuation(noEnts)
+    noNum = removeNumbers(noPunct)
+    prep = np.array(noNum)
+
+    if batch_size > len(prep):
+        return None
+
+    excess = len(prep) % batch_size
+
+    if(excess == 0):
+        batches = np.reshape(prep, (-1, batch_size))
+    else:
+        batches = np.reshape(prep[:-excess], (-1, batch_size))
+
+    lemmaNumbers = list()
+    for batch in batches:
+        lemmas = set()
+        for token in batch:
+            lemmas.add(token.lemma_)
+        lemmaNumbers.append(len(lemmas))
+
+    return sum(lemmaNumbers)/len(lemmaNumbers)/batch_size
+
+
+def getChatStrings(df: pd.DataFrame, chat: str, avg_batch_chars: int = 70000) -> dict:
+    prep = df.dropna(subset=["content"])
+    oneChat = prep[prep["chat_with"] == chat]
+
+    chatStrings = dict()
+    for direction in ["Sent", "Received"]:
+        directedChat = oneChat[oneChat["message_direction"] == direction]
+        messages = np.array(directedChat["content"].astype(str).values)
+
+        totalLen = sum([len(message) for message in messages])
+        batchNum = math.ceil(totalLen/avg_batch_chars)
+
+        batches = np.array_split(messages, batchNum)
+
+        batchStrings = [". ".join(batchMessages) for batchMessages in batches]
+
+        chatStrings[direction] = batchStrings
+
+    return chatStrings
--- readme.md
+++ readme.md
@@ -2,6 +2,16 @@
 
 For many of us messenger is the main communicator. The text we send and receive may say a lot about us, and our relationships. Analysing own data may be a great step towards self awareness.
 
+## Usage
+
+Clone this repository setup parameters in `data_cleaning.py` and `exploration.py` and run:
+
+```bash
+pip install -r requirements.txt
+python -m spacy download pl_core_news_md
+python -m spacy download en_core_web_sm
+```
+
 ## Contribute
 
 This repository is under heavy maintenance. Consider contributing and helping with the creation of a tool that may be used by many.
